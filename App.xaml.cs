3e5183b48d8654890ce38309aaefc17262dedd24
inetconnector <frede@inetconnector.com>
2025-12-25 20:14:02 +0100

    Fix app resource initialization ordering

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,12 +1,15 @@
+using Microsoft.Extensions.DependencyInjection;
 using UltimateVideoBrowser.Views;
 
 namespace UltimateVideoBrowser;
 
 public partial class App : Application
 {
-    public App(MainPage mainPage)
+    public App(IServiceProvider serviceProvider)
     {
         InitializeComponent();
+
+        var mainPage = serviceProvider.GetRequiredService<MainPage>();
         MainPage = new NavigationPage(mainPage);
     }
 }
