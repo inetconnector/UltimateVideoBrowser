596b60f57ad2e4127173cac57100e87d1130b917
inetconnector <frede@inetconnector.com>
2025-12-25 22:22:55 +0100

    Test fixes

--- UltimateVideoBrowser.Tests/SourcesViewModelTests.cs
+++ UltimateVideoBrowser.Tests/SourcesViewModelTests.cs
@@ -1,3 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Maui;
 using Microsoft.Maui.Controls;
 using UltimateVideoBrowser.Models;
 using UltimateVideoBrowser.Services;
