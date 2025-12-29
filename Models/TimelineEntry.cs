37534ea12f18930681e2f6d89c606a42fc74bd40
inetconnector <frede@inetconnector.com>
2025-12-29 15:42:49 +0100

    Update timeline thumbnails when video previews load

--- Models/TimelineEntry.cs
+++ Models/TimelineEntry.cs
@@ -1,19 +1,25 @@
+using System.ComponentModel;
 using System.Globalization;
+using System.Runtime.CompilerServices;
 
 namespace UltimateVideoBrowser.Models;
 
-public class TimelineEntry
+public class TimelineEntry : INotifyPropertyChanged
 {
+    private readonly VideoItem anchorVideo;
+
     public TimelineEntry(int year, int month, VideoItem anchorVideo, bool showYear)
     {
         Year = year;
         Month = month;
-        AnchorVideo = anchorVideo;
+        this.anchorVideo = anchorVideo;
         ShowYear = showYear;
 
         var date = new DateTime(year, month, 1);
         MonthLabel = date.ToString("MMM", CultureInfo.CurrentCulture).ToUpperInvariant();
         YearLabel = year.ToString(CultureInfo.InvariantCulture);
+
+        this.anchorVideo.PropertyChanged += OnAnchorVideoPropertyChanged;
     }
 
     public int Year { get; }
@@ -21,5 +27,19 @@ public class TimelineEntry
     public string MonthLabel { get; }
     public string YearLabel { get; }
     public bool ShowYear { get; }
-    public VideoItem AnchorVideo { get; }
+    public VideoItem AnchorVideo => anchorVideo;
+    public string? ThumbnailPath => anchorVideo.ThumbnailPath;
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    private void OnAnchorVideoPropertyChanged(object? sender, PropertyChangedEventArgs e)
+    {
+        if (e.PropertyName == nameof(VideoItem.ThumbnailPath))
+            OnPropertyChanged(nameof(ThumbnailPath));
+    }
+
+    private void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
 }
