63fc94fa7d5fc724e08604d87a0a6cc99fe79322
inetconnector <frede@inetconnector.com>
2026-01-01 20:26:01 +0100

    Expose timeline scroll handlers

---
+++ Views/Components/MainHighlightsView.xaml.cs
@@ -0,0 +1,9 @@
+namespace UltimateVideoBrowser.Views.Components;
+
+public partial class MainHighlightsView : ContentView
+{
+    public MainHighlightsView()
+    {
+        InitializeComponent();
+    }
+}
