de909179b02f51e4205c8ac319327385f79afbd7
inetconnector <frede@inetconnector.com>
2026-01-01 20:04:59 +0100

    Add timeline scroll handler stub

---
+++ Views/Components/PeopleTaggedHighlightView.xaml.cs
@@ -0,0 +1,9 @@
+namespace UltimateVideoBrowser.Views.Components;
+
+public partial class PeopleTaggedHighlightView : ContentView
+{
+    public PeopleTaggedHighlightView()
+    {
+        InitializeComponent();
+    }
+}
