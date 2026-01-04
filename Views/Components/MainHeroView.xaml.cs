da29be25198ff5689f4b6be6d47d0655fe95b123
inetconnector <frede@inetconnector.com>
2026-01-05 00:55:08 +0100

    Remove unsupported Frame.ContextFlyout

--- Views/Components/MainHeroView.xaml.cs
+++ Views/Components/MainHeroView.xaml.cs
@@ -1,3 +1,5 @@
+using UltimateVideoBrowser.Resources.Strings;
+
 namespace UltimateVideoBrowser.Views.Components;
 
 public partial class MainHeroView : ContentView
@@ -5,5 +7,31 @@ public partial class MainHeroView : ContentView
     public MainHeroView()
     {
         InitializeComponent();
+        ConfigureActionsFlyout();
+    }
+
+    private void OnActionsClicked(object sender, EventArgs e)
+    {
+        if (sender is VisualElement element)
+        {
+            FlyoutBase.ShowAttachedFlyout(element);
+        }
+    }
+
+    private void ConfigureActionsFlyout()
+    {
+        var flyout = new MenuFlyout();
+        flyout.Add(CreateFlyoutItem(AppResources.SourcesButton, "OpenSourcesCommand"));
+        flyout.Add(CreateFlyoutItem(AppResources.AlbumsButton, "OpenAlbumsCommand"));
+        flyout.Add(CreateFlyoutItem(AppResources.SettingsButton, "OpenSettingsCommand"));
+        flyout.Add(CreateFlyoutItem(AppResources.ReindexButton, "RunIndexCommand"));
+        FlyoutBase.SetAttachedFlyout(ActionsButton, flyout);
+    }
+
+    private static MenuFlyoutItem CreateFlyoutItem(string text, string commandPath)
+    {
+        var item = new MenuFlyoutItem { Text = text };
+        item.SetBinding(MenuFlyoutItem.CommandProperty, commandPath);
+        return item;
     }
 }
